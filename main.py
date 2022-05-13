a3c5e83e011cbe0de3d6e3fc0515f48ef13d6c0e
JAco9307 <skrivtiljacob@hotmail.com>
2022-05-13 17:54:52 +0200

    Imports

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import tensorflow as tf
+import numpy as np
+from tensorflow.python import keras
+import os
+import cv2
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.preprocessing import image
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    train_dataset = train.flow_from_directory("C:/Users/skriv/PycharmProjects/MDB1/Samples_images")
+    test_dataset = train.flow_from_directory("C:/Users/skriv/PycharmProjects/MDB1/Test_images")
