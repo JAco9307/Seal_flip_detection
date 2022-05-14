8caac69fe08993e08c9c73e4e8a82dc90c702f9f
JAco9307 <skrivtiljacob@hotmail.com>
2022-05-14 16:56:34 +0200

    Some memory improvements, still not enough tho

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
@@ -6,15 +7,17 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
+    size = (640, 360)
     train = ImageDataGenerator(rescale=1/255)
     test = ImageDataGenerator(rescale=1/255)
-    train_dataset = train.flow_from_directory("C:/Users/skriv/PycharmProjects/MDB1/Samples_images")
-    test_dataset = test.flow_from_directory("C:/Users/skriv/PycharmProjects/MDB1/Test_images")
+
+    train_dataset = train.flow_from_directory("C:/Users/skriv/PycharmProjects/MDB1/Training_W/Train_white")
+    test_dataset = test.flow_from_directory("C:/Users/skriv/PycharmProjects/MDB1/Training_W/Test_white")
 
     model = keras.Sequential()
 
     # Convolutional layer and maxpool layer 1
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
     model.add(keras.layers.MaxPool2D(2, 2))
 
     # Convolutional layer and maxpool layer 2
@@ -22,30 +25,33 @@ if __name__ == '__main__':
     model.add(keras.layers.MaxPool2D(2, 2))
 
     # Convolutional layer and maxpool layer 3
-    # model.add(keras.layers.Conv2D(128,(3,3),activation='relu'))
-    # model.add(keras.layers.MaxPool2D(2,2))
+    model.add(keras.layers.Conv2D(128,(3,3),activation='relu'))
+    model.add(keras.layers.MaxPool2D(2,2))
 
     # Convolutional layer and maxpool layer 4
-    # model.add(keras.layers.Conv2D(128,(3,3),activation='relu'))
-    # model.add(keras.layers.MaxPool2D(2,2))
+    model.add(keras.layers.Conv2D(128,(3,3),activation='relu'))
+    model.add(keras.layers.MaxPool2D(2,2))
 
     # This layer flattens the resulting image array to 1D array
     model.add(keras.layers.Flatten())
 
     # Hidden layer with 512 neurons and Rectified Linear Unit activation function
-    model.add(keras.layers.Dense(512, activation='relu'))
+    model.add(keras.layers.Dense(256, activation='relu'))
 
     # Output layer with single neuron which gives 0 for Cat or 1 for Dog
     # Here we use sigmoid activation function which makes our model output to lie between 0 and 1
     model.add(keras.layers.Dense(1, activation='sigmoid'))
 
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    model.fit_generator(train_dataset, steps_per_epoch=20, epochs=5, validation_data=test_dataset)
+    model.compile(loss='binary_crossentropy',optimizer=keras.optimizers.RMSprop(learning_rate=0.001), metrics='accuracy')
+
+    # model.fit_generator(train_dataset, steps_per_epoch=20, epochs=5, validation_data=test_dataset)
+    history = model.fit(train_dataset, steps_per_epoch=20, batch_size=16, epochs=15, verbose=1, validation_data=test_dataset, validation_steps=80)
 
 
     def predict_image(filename):
-        img1 = image.load_img(filename, target_size=(150, 150))
+        img1 = image.load_img(filename, target_size=(256, 256))
 
         plt.imshow(img1)
 
